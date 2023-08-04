f8c0b108daae4a82a0aac749ef8a69953952ac31
Autalyst <Aut.Baldwin@gmail.com>
2023-08-03 23:19:55 -0500

    Removed cookie support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import './polyfill';
-import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
       transform: true,
       whitelist: true, // strips out undefined variables
