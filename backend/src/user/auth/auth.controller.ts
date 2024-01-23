91835af462f140e19e4f5e874a3cedef7006b497
Autalyst <Aut.Baldwin@gmail.com>
2024-01-23 00:52:01 -0600

    Removed unused import from auth controller (get, not used)

--- backend/src/user/auth/auth.controller.ts
+++ backend/src/user/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
 import { UserAuthService } from './auth.service';
 import { Public } from './auth.decorator';
 import { AuthRequestDto } from './dto/auth.request.dto';
